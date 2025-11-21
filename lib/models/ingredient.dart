02e3540ae74e4f7883a87c6b748dcc78a7223ced
Aaron Museo Nzangi <132920661+AaronMuseo@users.noreply.github.com>
2025-11-21 22:47:09 +0300

    Update ingredient.dart

--- lib/models/ingredient.dart
+++ lib/models/ingredient.dart
@@ -1,1 +1,24 @@
+class Ingredient {
+  final String id;
+  final String name;
+  final int inventoryLevel;
 
+  Ingredient({
+    required this.id,
+    required this.name,
+    required this.inventoryLevel,
+  });
+
+  factory Ingredient.fromMap(Map<String, dynamic> map, String id) {
+    return Ingredient(
+      id: id,
+      name: map['name'] ?? '',
+      inventoryLevel: map['inventoryLevel'] ?? 0,
+    );
+  }
+
+  Map<String, dynamic> toMap() => {
+    'name': name,
+    'inventoryLevel': inventoryLevel,
+  };
+}
